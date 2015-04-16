78cd990b6b7b58b3ebaf091b9bab8376956745a3
kenice washington <kenicek56@gmail.com>
2015-04-16 16:30:43 -0700

    finished video 5.

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 if($mysqli->connect_error) {
 die('Connect Error{'. $mysqli->connect_errno . '}'
 	. $mysqli->connect_error);
