1713524d9ce80ce0f915256588e4f95f27afcacb
kenice washington <kenicek56@gmail.com>
2015-04-16 16:53:10 -0700

    finished video 6.

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
- <div class="wrap">
-	
- </div>
+  <div class="wrap">
+	<div class="task-list">
+		<ul>
+			<?php require("includes/connect.php"); ?>
+		</ul>
+	</div>
+  <form class="add-new-task" autocomplete="off">
+  <input type="text" name="new-task" placeholder="Add new item..."/>
+  </form>
+  </div>
 </body>
 </html>
