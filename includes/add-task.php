b6b737182a38be61d5207f30703e8860122bba34
kenice washington <kenicek56@gmail.com>
2015-04-16 18:23:00 -0700

    finished video 12

--- includes/add-task.php
+++ includes/add-task.php
@@ -10,4 +10,14 @@ $mysqli ->query("INSERT INTO task VALUES ('', '$task', '$date', '$time')");
 
 $query = "SELECT * FROM task WHERE task='$task' and date='$date' and time='$time' "; 
 
+if($result = $mysqli->query($query)) {
+	while ($row = $result->fetch_assoc()) {
+		$task_id = $row['id'];
+		$task_name = $row['task'];
+	}
+}
+
+$mysqli->close();
+
+echo '<li><span>'.$task_name.'</span><img id="'.$task_id.'" class="delete-button" width="18px" src="images/close" /></li>';
 ?>
