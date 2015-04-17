e2a52aab10a4e23dae44cd6f7848732e5d552877
kenice washington <kenicek56@gmail.com>
2015-04-16 18:10:09 -0700

    finished video 11

---
+++ includes/add-task.php
@@ -0,0 +1,13 @@
+<?php
+$task = strip_tags($_POST['task']);
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+include('connect.php');
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli ->query("INSERT INTO task VALUES ('', '$task', '$date', '$time')");
+
+$query = "SELECT * FROM task WHERE task='$task' and date='$date' and time='$time' "; 
+
+?>
